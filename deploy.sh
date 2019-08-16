1f2f3d7fd0e050f0013c4b5b8302f2920307fefb
proactive-developer <proactive.job@outlook.com>
2019-08-16 19:54:40 +0800

    Add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git config user.name 'proactive-developer'
+git config user.email 'proactive.job@outlook.com'
+git add -A
+git commit -m 'Deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:proactive-developer/portfolio.git master:gh-pages
+
+cd -
